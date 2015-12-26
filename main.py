e5a1263a788f1a04494d22c6eb25d5ab344b510c
root <root@hamming.urgu.org>
2015-12-26 21:09:04 +0500

    Added simple deployment method.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 
 from common import config
-from methods import amt, test
+from methods import amt, simple, test
 
 def main(raw_args):
-    return config.execute_with(raw_args, [amt.AMTMethod, test.TestMethod])
+    return config.execute_with(
+        raw_args, [amt.AMTMethod, simple.SimpleMethod, test.TestMethod])
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
---
+++ methods/simple.py
@@ -0,0 +1,28 @@
+import datetime
+
+from common import method
+from stages import basic, boot, config, ndd, network, slurm, ssh
+
+class SimpleMethod(method.Method):
+    name = 'simple'
+
+    stages = [
+        basic.InitHosts(),
+        basic.ExcludeBannedHosts(),
+        ssh.CheckIsAccessibleViaSSH(*ssh.Timeouts.TINY),
+        boot.SetBootIntoCOWMemory(),
+        ssh.RebootHost(*ssh.Timeouts.TINY),
+        ssh.WaitUntilBootedIntoCOWMemory(*ssh.Timeouts.NORMAL),
+        boot.ResetBoot(),
+        config.StoreCOWConfig(),
+        network.EnsureNetworkSpeed(),
+        slurm.WaitForSlurmAvailable(*slurm.Timeouts.NORMAL),
+        ndd.RunNDDViaSlurm(),
+        config.CopySSHCredentialsIntoWindows7Partition(),
+        boot.SetBootIntoLocalWin7(),
+        ssh.RebootLinuxHost(*ssh.Timeouts.TINY),
+        ssh.WaitUntilBootedIntoWindows(*ssh.Timeouts.BIG),
+        boot.ResetBoot(),
+        ssh.RebootWindowsHost(*ssh.Timeouts.TINY),
+        ssh.WaitForSSHAvailable(*ssh.Timeouts.NORMAL),
+    ]
