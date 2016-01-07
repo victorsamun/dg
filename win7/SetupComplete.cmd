ccf68a1027a025c2e979d0f1e98ea074f27df297
root <root@hamming.urgu.org>
2016-01-07 23:30:51 +0500

    Disable auto updates in SetupComplete.cmd

--- win7/SetupComplete.cmd
+++ win7/SetupComplete.cmd
@@ -1,2 +1,7 @@
+rem This is to disable auto updates
+reg add ^
+  "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" ^
+  /v AUOptions /t REG_DWORD /d 1 /f
+
 net user Administrator /active:yes
 net start sshd
