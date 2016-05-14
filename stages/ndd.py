274e0577fcf11a70c15a5b3aa4703c8d54c93b39
root <root@hamming.urgu.org>
2016-05-14 19:49:02 +0500

    Added a stage to run ndd with ssh instead of slurm.

--- stages/ndd.py
+++ stages/ndd.py
@@ -17,3 +17,23 @@ class RunNDDViaSlurm(config.WithLocalAddress, config.WithNDDArgs, stage.Stage):
             if rv != 0:
                 for host in list(state.active_hosts):
                     host.fail(self, 'failed to run ndd_slurm.py')
+
+
+class RunNDD(config.WithLocalAddress, config.WithNDDArgs,
+             config.WithSSHCredentials, stage.Stage):
+    'deploy the images with ndd'
+
+    def run(self, state):
+        for src, dst in self.ndds:
+            cmdline = ['python', '/usr/local/bin/ndd_slurm.py']
+            cmdline.extend(['-H', '-s', self.local_addr, '-i', src, '-o', dst])
+
+            for host in sorted(state.active_hosts,
+                               key=lambda host: host.props.get('switch')):
+                cmdline.extend(
+                    ['-d', '{}@{}'.format(self.get_login(), str(host.sname))])
+
+            rv, _ = proc.run_process(cmdline, state.log)
+            if rv != 0:
+                for host in list(state.active_hosts):
+                    host.fail(self, 'failed to run ndd_slurm.py')
