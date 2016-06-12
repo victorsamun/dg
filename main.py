5aac694aec7f8ab568a78b39501c71f2d319cc57
root <root@hamming.urgu.org>
2016-06-12 22:52:07 +0500

    Remove test method.
    Now we can select steps from other methods while testing.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 
 from common import config
-from methods import amt, simple, single, stdm, test
+from methods import amt, simple, single, stdm
 
 def main(raw_args):
     return config.execute_with(
         raw_args, [amt.AMTMethod, simple.SimpleMethod, single.SingleMethod,
-                   stdm.StdMMethod, test.TestMethod])
+                   stdm.StdMMethod])
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
